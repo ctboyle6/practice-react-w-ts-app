ca78cc54c0ba016e9d724dd0aac240f09f905e11
Chris Boyle <cthboyle6@gmail.com>
2021-12-28 10:44:05 -0500

    built initial Todo List component and connected to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+
 const App: React.FC = () => {
   return (
     <div className="App">
-
+      <TodoList />
     </div>
   );
 }
