ce71e210829a2c7254ac0f393be62a55c2a2f46b
Chris Boyle <cthboyle6@gmail.com>
2021-12-28 11:54:31 -0500

    add Todo function and handle in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import TodoList from './components/TodoList';
-import NewTodo from './components/NewTodo';
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // [{ id: "t1", text: "Finish the course" }]
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
-      <TodoList items={todos}/>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
-}
+};
 
 export default App;
