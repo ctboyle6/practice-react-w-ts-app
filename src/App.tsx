a8f3a2425c094ac1a2dbba746a98d84729423474
Chris Boyle <cthboyle6@gmail.com>
2021-12-28 10:54:37 -0500

    linked todo items to TodoList props with interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course" }];
   return (
     <div className="App">
-      <TodoList />
+      {/* A component to add todos here */}
+      <TodoList items={todos}/>
     </div>
   );
 }
